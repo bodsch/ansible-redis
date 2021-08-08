20eab819d3eff87b77b3caab6dd97382e06a92e4
Bodo Schulz <bodo@boone-schulz.de>
2021-08-08 14:15:34 +0200

    fix default test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,9 +10,10 @@ import os
 
 import testinfra.utils.ansible_runner
 
+HOST = 'instance'
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts(HOST)
 
 
 def pp_json(json_thing, sort=True, indents=2):
@@ -37,27 +38,59 @@ def base_directory():
     return directory, molecule_directory
 
 
+def read_ansible_yaml(file_name, role_name):
+    ext_arr = ["yml", "yaml"]
+
+    read_file = None
+
+    for e in ext_arr:
+        test_file = "{}.{}".format(file_name, e)
+        if os.path.isfile(test_file):
+            read_file = test_file
+            break
+
+    return "file={} name={}".format(read_file, role_name)
+
+
+
 @pytest.fixture()
 def get_vars(host):
     """
         parse ansible variables
         - defaults/main.yml
         - vars/main.yml
+        - vars/${DISTRIBUTION}.yaml
         - molecule/${MOLECULE_SCENARIO_NAME}/group_vars/all/vars.yml
     """
     base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
 
-    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
-    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
-    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
+    if distribution in ['debian', 'ubuntu']:
+        os = "debian"
+    elif distribution in ['centos', 'redhat', 'ol']:
+        os = "redhat"
+    elif distribution in ['arch']:
+        os = "archlinux"
 
-    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
-    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
-    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+    print(" -> {} / {}".format(distribution, os))
+
+    file_defaults      = read_ansible_yaml("{}/defaults/main".format(base_dir)          , "role_defaults")
+    file_vars          = read_ansible_yaml("{}/vars/main".format(base_dir)              , "role_vars")
+    file_distibution   = read_ansible_yaml("{}/vars/{}".format(base_dir, os)            , "role_distibution")
+    file_molecule      = read_ansible_yaml("{}/group_vars/all/vars".format(base_dir)    , "test_vars")
+    # file_host_molecule = read_ansible_yaml("{}/host_vars/{}/vars".format(base_dir, HOST), "host_vars")
+
+    defaults_vars      = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars          = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars   = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
+    molecule_vars      = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+    # host_vars          = host.ansible("include_vars", file_host_molecule).get("ansible_facts").get("host_vars")
 
     ansible_vars = defaults_vars
     ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
     ansible_vars.update(molecule_vars)
+    # ansible_vars.update(host_vars)
 
     templar = Templar(loader=DataLoader(), variables=ansible_vars)
     result = templar.template(ansible_vars, fail_on_undefined=False)
